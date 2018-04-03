e855e853539c85e368a6387b0cd47d341c313b62
NProof <34260623+NProof@users.noreply.github.com>
2018-04-03 16:49:41 +0800

    target

--- Main.cpp
+++ Main.cpp
@@ -1,50 +1,63 @@
 #include <iostream>
 
-#include <set>
-
 #include "Broad.h"
-#include "Chess.h"
-#include "Check.h"
-#include "Move.h"
-
-//class Move;
+#include "Ai.h"
 
 using namespace std;
 
 int main()
 {
     Broad b;
-    char c[35] = "XnXXC-XXXXXXcXpXM-XXXkXMXXXpXXX";
+	Ai ai1, ai2;
+
+	ai1.broadOfAi = &b;
+	ai2.broadOfAi = &b;
+
+	char move[6] ;
+
+//    char c[64] = "XnXXC-XXXXXXcXpXM-XXRkXMXXXpXXXG";
+    char c[64] = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
     b.initBroad(c);
 
-//    cout << b << endl;
-//    cout << &b.checks[3] << endl;
-//    cout << b.checks[3].chess << endl;
-//    cout << b.checks[3].chess->check << endl;
+//    while(ai1.generateMove(move)&&ai1.MakeMove(move)&&ai2.generateMove(move)&&ai2.MakeMove(move));
+    while(true)
+    {
+        cout << b << endl;
+        if(ai1.generateMove(move))
+        {
+            if(b.mapOfName[std::string{move[0], move[1]}]==b.mapOfName[std::string{move[0], move[1]}])
+            {
+                scanf("%s", &move[3]);
+                move[2] = '('; move[4] = ')';
+            }
+            printf("%s\n", move);
+        }
+        else
+            break;
 
-//**//
-//    char op[6];
-    /* Flip * flip = new Flip(b.checks[4].chess) ;
-    cout << *flip->fliped << endl;
-    flip->generateMove(op);
-    cout << op << endl;
+        if(ai1.MakeMove(move))
+            printf("%s\n", move);
+        else
+            break;
 
-    Shift * shift = new Shift(b.checks[15].chess, &b.checks[14]);
-    shift->generateMove(op);
-    cout << op << endl; */
+        cout << b << endl;
+        if(ai2.generateMove(move))
+        {
+            if(b.mapOfName[std::string{move[0], move[1]}]==b.mapOfName[std::string{move[0], move[1]}])
+            {
+                scanf("%s", &move[3]);
+                move[2] = '('; move[4] = ')';
+            }
+            printf("%s\n", move);
+        }
+        else
+            break;
 
-    char op[6];
-    // Move move(b.checks[10].chess, nullptr);
-    // move.generateMove(op);
-    cout << op << " and Move ... " << endl;
-	cout << b << endl;
-	cout << *b.checks[4].chess << endl;
-	b.checks[4].chess->pickon();
-	std::map<Path, Move *> ioo = b.checks[4].chess->mapOfMoves;
-	cout << ioo.size() << endl;
-	std::map<Path, Move *>::iterator it;
-	// for(it = ioo.begin(); it != ioo.end(); it++)
-		// std::cout << " " << it->second->chess->check->name << " , " << it->second->check->name << std::endl;
+        if(ai2.MakeMove(move))
+            printf("%s\n", move);
+        else
+            break;
+    }
 
 	return 0;
 }
