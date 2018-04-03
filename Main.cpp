9f2a42ba70e804a927efe041c56c8d52e23c3fc8
NProof <34260623+NProof@users.noreply.github.com>
2018-04-03 16:51:55 +0800

    update target

--- Main.cpp
+++ Main.cpp
@@ -1,17 +1,7 @@
 #include <iostream>
 
-#include <vector>
-#include <set>
-
 #include "Broad.h"
-#include "Chess.h"
-#include "Check.h"
-#include "Move.h"
 #include "Ai.h"
-
-#include <cstdlib>
-
-class Check;
 
 using namespace std;
 
@@ -25,17 +15,48 @@ int main()
 
 	char move[6] ;
 
-    char c[35] = "XnXXC-XXXXXXcXpXM-XXRkXMXXXpXXXG";
-    b.initBroad(nullptr);
+//    char c[64] = "XnXXC-XXXXXXcXpXM-XXRkXMXXXpXXXG";
+    char c[64] = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+    b.initBroad(c);
 
 //    while(ai1.generateMove(move)&&ai1.MakeMove(move)&&ai2.generateMove(move)&&ai2.MakeMove(move));
     while(true)
     {
         cout << b << endl;
-        if(ai1.generateMove(move)&&ai1.MakeMove(move)&&ai2.generateMove(move)&&ai2.MakeMove(move))
-            printf("dd\n");
+        if(ai1.generateMove(move))
+        {
+            if(b.mapOfName[std::string{move[0], move[1]}]==b.mapOfName[std::string{move[0], move[1]}])
+            {
+                scanf("%s", &move[3]);
+                move[2] = '('; move[4] = ')';
+            }
+            printf("%s\n", move);
+        }
         else
-            printf("fail\n");
+            break;
+
+        if(ai1.MakeMove(move))
+            printf("%s\n", move);
+        else
+            break;
+
+        cout << b << endl;
+        if(ai2.generateMove(move))
+        {
+            if(b.mapOfName[std::string{move[0], move[1]}]==b.mapOfName[std::string{move[0], move[1]}])
+            {
+                scanf("%s", &move[3]);
+                move[2] = '('; move[4] = ')';
+            }
+            printf("%s\n", move);
+        }
+        else
+            break;
+
+        if(ai2.MakeMove(move))
+            printf("%s\n", move);
+        else
+            break;
     }
 
 	return 0;
