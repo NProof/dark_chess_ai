343e524f0bf81d724e50c8db112abd54f73154fc
NProof <34260623+NProof@users.noreply.github.com>
2018-04-04 16:38:02 +0800

    Vesion 0-01

---
+++ include/Player.h
@@ -0,0 +1,19 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include <cstdio>
+
+class Player
+{
+    public:
+        Player();
+        virtual ~Player();
+
+        void generateMove(char *);
+
+    protected:
+
+    private:
+};
+
+#endif // PLAYER_H
